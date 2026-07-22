5725e14029ad83d092e8af79f180c9d81c119fb2
SignalASI <signalasi@hotmail.com>
2026-07-23 04:35:36 +0800

    Keep context journal test forward compatible

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.signalasi.chat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 155
-        versionName = "0.1.154"
+        versionCode = 156
+        versionName = "0.1.155"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
