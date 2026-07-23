f0abd8f65fcb5e8fe961dcbbe89b90f837e80fbf
SignalASI <signalasi@hotmail.com>
2026-07-24 06:43:58 +0800

    Delete chat history durably

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.signalasi.chat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 179
-        versionName = "0.1.178"
+        versionCode = 186
+        versionName = "0.1.185"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
