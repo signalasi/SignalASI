057539a2b0401fd1a3b2d5bc3f60f62553ea442d
SignalASI <signalasi@hotmail.com>
2026-07-24 06:31:58 +0800

    Recover Android MQTT subscriptions

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.signalasi.chat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 179
-        versionName = "0.1.178"
+        versionCode = 185
+        versionName = "0.1.184"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
