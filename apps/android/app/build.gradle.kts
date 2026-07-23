1ec983ea0e904e94a7df9ae9e7282c37b43dd908
SignalASI <signalasi@hotmail.com>
2026-07-23 20:04:55 +0800

    Persist Android background messages durably

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.signalasi.chat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 168
-        versionName = "0.1.167"
+        versionCode = 172
+        versionName = "0.1.171"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
